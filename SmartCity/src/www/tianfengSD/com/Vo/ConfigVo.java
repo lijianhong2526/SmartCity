1fb7c9430cae55899aa9e57971550495584608ab
lijianhong2526 <545445896@qq.com>
2016-07-09 17:16:24 +0800

    添加系统配置VO

---
+++ SmartCity/src/www/tianfengSD/com/Vo/ConfigVo.java
@@ -0,0 +1,51 @@
+package www.tianfengSD.com.Vo;
+
+/**
+ * 系统配置
+ * 
+ * @author lijianhong
+ *
+ */
+public class ConfigVo extends BaseVo {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int configId;
+	private String configDes;
+	private String configValue;
+	private String configType;
+
+	public int getConfigId() {
+		return configId;
+	}
+
+	public void setConfigId(int configId) {
+		this.configId = configId;
+	}
+
+	public String getConfigDes() {
+		return configDes;
+	}
+
+	public void setConfigDes(String configDes) {
+		this.configDes = configDes;
+	}
+
+	public String getConfigValue() {
+		return configValue;
+	}
+
+	public void setConfigValue(String configValue) {
+		this.configValue = configValue;
+	}
+
+	public String getConfigType() {
+		return configType;
+	}
+
+	public void setConfigType(String configType) {
+		this.configType = configType;
+	}
+}
